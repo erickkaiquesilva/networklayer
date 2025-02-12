2fd3bdeb3b6a9fdea2845d4c624c15f811c23bbb
erickkaiquesilva <erickkay272@gmail.com>
2025-02-12 17:15:52 -0300

    [feat: configSPM] configuration pastes and files to networklayer module.

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Magalu on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
